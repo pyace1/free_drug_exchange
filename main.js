6a10c80065d922b05de2cc13630d528bd135ecbc
Your Name <you@example.com>
2020-04-18 21:00:55 +0200

    ebancije

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-//slippry comment
-//srecan ti rodjendan dzohusafet
-//dje si aco skupljacu vintage satova
-//evo tu sam Joco frajeru sa citizenom
-//eco drive + automatic diver
-//opasan sat, nema zajebancije
+
 $(document).ready(function(){
   $('#slippry-demo').slippry()
 });
+var lista = [];
+
 function unos(){
-	var lek = document.getElementById("lek").value;
-	var rok = document.getElementById("rok").value;
-	var kolicina = document.getElementById("kolicina").value;
-	var ime = document.getElementById("ime").value;
-	var jmbg = document.getElementById("jmbg").value;
-	var telefon = document.getElementById("telefon").value;
-	var email = document.getElementById("email").value;
-	var lokacija = document.getElementById("lokacija").value;
-	var slanje = document.getElementById("slanje").value;
-	var napomena = document.getElementById("napomena").value;
-	document.getElementById("stampa").innerHTML = "<strong>" + lek + "</strong>&nbsp;" + rok + "&nbsp;" + kolicina + "</br>" + 
-	ime + "&nbsp;" + telefon + "&nbsp;" + email + "<br/><strong>" + lokacija + "</strong>" + "&nbsp;" + slanje + "<br/>" + napomena;
 	
-	
+	var medikament = {
+		lek : document.getElementById("lek").value,
+		rok : document.getElementById("rok").value,
+		kolicina : document.getElementById("kolicina").value,
+		produkt : document.getElementById("produkt").value,
+		ime : document.getElementById("ime").value,
+		jmbg : document.getElementById("jmbg").value,
+		telefon : document.getElementById("telefon").value,
+		email : document.getElementById("email").value,
+		lokacija : document.getElementById("lokacija").value,
+		//slanje : document.getElementByName("mybox").value,
+		napomena : document.getElementById("napomena").value,
+
+	}
+	lista.push(medikament);
+	console.log(lista);
+	var i;
+	for (i=0;i<lista.length;i++){
+	document.getElementById("stampa").innerHTML += "<div>" + lista[i].ime + "</div>"; 
+	}
 }
+
